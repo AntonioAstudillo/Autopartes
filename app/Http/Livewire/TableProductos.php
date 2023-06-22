bf8f81ef720d34f3f7831da6c8442e6af2f6efcb
desarrollo@hoautopartes.mx <artemiokaoz@gmail.com>
2023-06-22 15:27:48 -0600

    Version 1 modulo editar producto

--- app/Http/Livewire/TableProductos.php
+++ app/Http/Livewire/TableProductos.php
@@ -11,16 +11,34 @@ class TableProductos extends Component
 {
 
     use WithPagination;
-    public $search = '';
-    public $modalData;
-    public $modalDataDetalle;
-    public $isShow;
-    protected $paginationTheme = 'bootstrap';
+    public $search = '';  //Variable usada para obtener el valor del input de busqueda
+    public $modalData; //Variable usada para almacenar la información de un producto y poder mostrarla en los respectivos modales
+    public $familias; //variable usada para almacenar todas las familias del sistema y de esa manera poder llenar los selects
+    public $grupos;  //variable usada para almacenar los grupos para poder llenar los selects
+    public $modalDataDetalle; //Almacenamos la informacion detalla de un producto y así poder llenar el table dentro del modal show
+    public $isShow , $isEdit , $isDelete; //Esta variables son banderas que uso para poder mostrar o esconder los modales
+    protected $paginationTheme = 'bootstrap'; //Determinamos que la paginacion tenga los estilos de bootstrap
 
-    protected $listeners = ['mostrarData' => 'showProducto'];
 
+    /** Data de los formularios */
 
+    public $codigo;
+    public $empaque;
+    public $grupo;
+    public $posicion;
+    public $tipoCubrePolvo;
+    public $familia;
+    public $tipoPiston;
+    public $lado;
+    public $uxv;
+    public $diametroInterior;
+    public $codigoEquivalente;
+    public $altura;
+    public $catalogo;
+    public $descripcion;
+    public $oem;
 
+    //Restablecemos el paginador para que no genere conflictos a la hora de buscar un producto
     public function updatingSearch()
     {
         $this->resetPage();
@@ -57,9 +75,71 @@ class TableProductos extends Component
     }
 
 
+    //Mostramos el modal de editar productos
+    public function editProducto($id){
+        $this->isEdit = true;
+        $this->modalData =  Productos::where('codigo' , '=' , $id)->first();
+
+        $this->codigo = $this->modalData->codigo;
+        $this->empaque = $this->modalData->empaque;
+        $this->grupo = $this->modalData->grupo;
+        $this->posicion = $this->modalData->posicion;
+        $this->tipoCubrePolvo = $this->modalData->tipoCubrePolvo;
+        $this->familia = $this->modalData->familia;
+        $this->tipoPiston = $this->modalData->tipoPiston;
+        $this->lado = $this->modalData->lado;
+        $this->uxv = $this->modalData->uxv;
+        $this->diametroInterior = $this->modalData->diametroInterior;
+        $this->codigoEquivalente = $this->modalData->codigoEquivalente;
+        $this->altura = $this->modalData->altura;
+        $this->catalogo = $this->modalData->catalogo;
+        $this->descripcion = $this->modalData->descripcion;
+        $this->oem = $this->modalData->oem;
+
+        $this->familias = DB::table('familias')->select('familia')->get();
+        $this->grupos = DB::table('subfamilias')->select('subfamilia')->get();
+    }
+
+
 
     public function closeModal(){
         $this->isShow = false;
+        $this->isEdit = false;
+    }
+
+    /**
+     * Este metodo se ejecuta una vez que el usuario haya presionado sobre el boton de guardar cambios dentro del modal editar producto
+     */
+
+    public function updateProducto(){
+
+        DB::table('productos')->where(['codigo' => $this->codigo])
+        ->update(
+            [
+                'empaque' => $this->empaque,
+                'familia' => $this->familia,
+                'grupo' => $this->grupo,
+                'descripcion' => $this->descripcion,
+                'posicion' => $this->posicion,
+                'tipoCubrePolvo' => $this->tipoCubrePolvo,
+                'tipoPiston' => $this->tipoPiston,
+                'lado' => $this->lado,
+                'empaque' => $this->empaque,
+                'uxv' => $this->uxv,
+                'diametroInterior' => $this->diametroInterior,
+                'codigoEquivalente' => $this->codigoEquivalente,
+                'oem' => $this->oem,
+                'altura' => $this->altura,
+                'catalogo' => $this->catalogo
+            ]
+        );
+
+        $this->closeModal();
+    }
+
+    public function deleteProducto($producto){
+        DB::table('productos')->where('codigo', '=', $producto)->delete();
+        DB::table('productodetalle')->where('producto', '=', $producto)->delete();
     }
 
 }
--- resources/views/livewire/table-productos.blade.php
+++ resources/views/livewire/table-productos.blade.php
@@ -43,9 +43,9 @@
                                 <td>
                                     <div class="container d-flex justify-content-center">
                                         <button wire:click="showProducto('{{$producto->codigo}}')" class="btn btn-sm btn-info mr-2" ><i class="fa-solid fa-eye fa-sm"></i> Mostrar</button>
-                                        <button class="btn btn-sm btn-warning mr-2" ><i class="fa-solid fa-pen-to-square fa-sm"></i>Editar</button>
-                                         <button class="btn btn-sm btn-primary mr-2" ><i class="fa-solid fa-camera fa-sm"></i>Imagen</button>
-                                        <button class="btn btn-sm btn-danger mr-2" ><i class="fa-solid fa-trash fa-sm"></i>Eliminar</button>
+                                        <button wire:click ="editProducto('{{$producto->codigo}}')" class="btn btn-sm btn-warning mr-2" ><i class="fa-solid fa-pen-to-square fa-sm"></i>Editar</button>
+                                        <button class="btn btn-sm btn-primary mr-2" ><i class="fa-solid fa-camera fa-sm"></i>Imagen</button>
+                                        <button wire:click ="deleteProducto('{{$producto->codigo}}')" class="btn btn-sm btn-danger mr-2" ><i class="fa-solid fa-trash fa-sm"></i>Eliminar</button>
                                     </div>
                                 </td>
                             </tr>
@@ -260,5 +260,149 @@
             </div>
         </div>
     @endif
+
+
+     @if ($isEdit)
+           <div class="modal" tabindex="-1" role="dialog" style="display: block;">
+            <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Información del producto</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close" wire:click="closeModal">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form wire:submit.prevent="updateProducto">
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="codigo">Código</label>
+                                    <input readonly type="text" name="codigo"  class="form-control" wire:model="codigo" >
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="empaque">Empaque</label>
+                                    <input type="text" name="empaque" class="form-control" wire:model="empaque" >
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputState">Grupo</label>
+                                    <select wire:model="grupo" id="inputState" class="form-control">
+
+                                        <option  value="{{$grupo}}" selected>{{{$grupo}}}</option>
+                                        @if(count($grupos)>0)
+                                            @foreach ($grupos as $grupoProducto )
+
+                                                @isset($grupoProducto->subfamilia)
+                                                    @if($grupoProducto->subfamilia !== $grupo)
+                                                        <option value="{{$grupoProducto->subfamilia}}">{{$grupoProducto->subfamilia}}</option>
+                                                    @endif;
+                                                @endisset
+                                            @endforeach
+                                        @endif
+                                        </select>
+                                </div>
+                                <div class="form-group col-md-6">
+                                <label>Posición</label>
+                                <input wire:model="posicion" type="text" class="form-control" value="{{$posicion}}">
+                            </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label>Tipo Cubre Polvo</label>
+                                    <input wire:model="tipoCubrePolvo" type="text" class="form-control" value="{{$tipoCubrePolvo}}">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="inputState">Familia</label>
+                                    <select  wire:model="familia" class="form-control">
+                                        <option value="{{$familia}}" selected>{{$familia}}</option>
+                                        @if(count($familias)>0)
+                                            @foreach ($familias as $familiaProducto )
+                                                @isset($familiaProducto->familia)
+                                                    @if($familiaProducto->familia !== $familia)
+                                                        <option value="{{$familiaProducto->familia}}">{{$familiaProducto->familia}}</option>
+                                                    @endif;
+                                                @endisset
+                                            @endforeach
+                                        @endif
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputZip">Tipo piston</label>
+                                    <input wire:model="tipoPiston"  type="text" class="form-control" value="{{$tipoPiston}}">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="inputZip">Lado</label>
+                                    <input  wire:model="lado" type="text" class="form-control" value="{{$lado}}" >
+                                </div>
+                            </div>
+
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputCity">UXV</label>
+                                    <input wire:model="uxv" type="text" class="form-control" value="{{$uxv}}"  id="inputCity">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="inputState">Diametro interior</label>
+                                     <input wire:model="diametroInterior" type="text" class="form-control" value="{{$diametroInterior}}"  id="inputCity">
+                                </div>
+                            </div>
+
+
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputZip">Código equivalente</label>
+                                    <input wire:model="codigoEquivalente" type="text" class="form-control" value="{{$codigoEquivalente}}"  id="inputZip">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="inputZip">Altura</label>
+                                    <input  wire:model="altura" type="text" class="form-control" value="{{$altura}}" id="inputZip">
+                                </div>
+                            </div>
+
+
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputZip">Catálogo</label>
+                                    <select wire:model="catalogo" class="form-control">
+                                        @if ($catalogo == 'Frenos')
+                                            <option  value="{{$catalogo}}" selected>{{$catalogo}}</option>
+                                            <option   value="Suspension">Suspension</option>
+
+                                        @else
+                                            <option  value="{{$catalogo}}" selected>{{$catalogo}}</option>
+                                            <option  value="Frenos">Frenos</option>
+                                        @endif
+
+                                    </select>
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="inputZip">OEM</label>
+                                    <input  wire:model="oem" type="text" class="form-control" value="{{$oem}}" id="inputZip">
+                                </div>
+                            </div>
+
+                            <div class="form-row">
+                                 <div class="form-group col-md-12">
+                                    <label for="exampleFormControlTextarea1">Descripción</label>
+                                    <textarea class="form-control" wire:model="descripcion" placeholder="No tiene descripción"></textarea>
+                                </div>
+                            </div>
+
+                            <div class="mt-3 d-flex justify-content-end">
+                                <input type="submit" class="btn btn-dark" value="Guardar Cambios">
+                            </div>
+
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+     @endif
 </div>
 <!-- /.container-fluid -->
